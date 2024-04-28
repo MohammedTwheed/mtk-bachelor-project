eae2c0bef7456b5d52cf59ec683b7d452668c84a
MohammedTwheed <MohammedTwheed@gmail.com>
2024-04-29 02:08:08 +0300

    returning probs_out to rescale

--- data/writings/posts/2024-04-15-trimming-draft-01/optimizeNNForTrimmingPumpImpeller.m
+++ data/writings/posts/2024-04-15-trimming-draft-01/optimizeNNForTrimmingPumpImpeller.m
@@ -1,4 +1,4 @@
-function [optimalHyperParams, finalMSE, randomSeed, bestTrainedNet] = optimizeNNForTrimmingPumpImpeller(x, t)
+function [optimalHyperParams, finalMSE, randomSeed, bestTrainedNet,probs_out] = optimizeNNForTrimmingPumpImpeller(x, t)
     % Function to optimize neural network hyperparameters for the trimming of a pump impeller.
     % Inputs:
     %   x - Input data (Q flowrate,H head) for the neural network.
@@ -11,8 +11,12 @@ function [optimalHyperParams, finalMSE, randomSeed, bestTrainedNet] = optimizeNN
 
     %EXAMPLE USAGE
     % first load the dataset using load('trimming_nn_training_dataset.mat')
-    % to find the optimum architecture for example call optimizeNNForTrimmingPumpImpeller(QH_nn_input',D_eta_nn_output')
-    %please make sure you do the transpose 
+    % to find the optimum architecture for example call optimizeNNForTrimmingPumpImpeller
+    % [a,b,c,d,probs_out]=optimizeNNForTrimmingPumpImpeller(QH_nn_input',D_eta_nn_output')
+    % probs_out is to use with mapminmax('reverse',bestTrainedNet(your
+    % input),probs_out) to get scale back since we have normalized it.
+    %please make sure you do the transpose if you are using our 'trimming_nn_training_dataset.mat'
+
 
     % Start timer to measure the duration of the optimization process.
     tic;
@@ -23,11 +27,16 @@ function [optimalHyperParams, finalMSE, randomSeed, bestTrainedNet] = optimizeNN
     activationFunctionOptions = {'tansig', 'logsig'};
 
     % Define bounds for the genetic algorithm optimization.
+    % the positional meaning [<hidden layer neurons number> ,< epochs>...
+    % ,<index of trainingFunctionOptions>,<index of activationFunctionOptions>]
     lowerBounds = [5, 50, 1, 1];
     upperBounds = [200, 200, 8, 2];
 
     % Define options for the genetic algorithm.
-    gaOptions = optimoptions('ga', 'MaxTime', 20);
+    % ConstraintTolerance, is the convergence limit its value determines
+    % the stop criteria for the ga.
+    % FitnessLimit, is the min mse value to stop search if mse get to it.
+    gaOptions = optimoptions('ga', 'MaxTime', 20,'ConstraintTolerance',0.003,'FitnessLimit',0.009);
 
     % Global variable to store the best trained neural network found during optimization.
     global bestTrainedNet;
@@ -44,7 +53,7 @@ function [optimalHyperParams, finalMSE, randomSeed, bestTrainedNet] = optimizeNN
     % SEI: just re train it MTK:!!
 
 
-    % Nested function to evaluate hyperparameters using the neural network.
+    % local function to evaluate hyperparameters using the neural network.
     function mse = evaluateHyperparameters(hyperParams, x, t, randomSeed)
         rng(randomSeed); % Set random seed for reproducibility.
 
@@ -60,16 +69,36 @@ function [optimalHyperParams, finalMSE, randomSeed, bestTrainedNet] = optimizeNN
         net.trainParam.epochs = maxEpochs;
         net.layers{1}.transferFcn = activationFunctionOptions{activationFunctionIdx};
 
+        % % b.c we don't want to apply mapminmax each time we call 
+        % % the function from a script (redundant)
+        % net.input.processFcns = {'removeconstantrows', 'mapminmax'};
+
+
+
+        % Normalize training data to [-1,1] this the defaul value 
+        % for mapminmax
+    
+        normalized_input = mapminmax(x);
+        [normalized_output,probs_out] = mapminmax(t);
+
+
         % Define data split for training, validation, and testing.
         net.divideParam.trainRatio = 0.7;
         net.divideParam.valRatio = 0.15;
         net.divideParam.testRatio = 0.15;
 
         % Train the neural network.
-        [trainedNet, ~] = train(net, x, t);
+        [trainedNet, ~] = train(net, normalized_input, normalized_output);
 
         % Evaluate the model performance using mean squared error (MSE).
-        predictions = trainedNet(x);
+
+        predictions = trainedNet(normalized_input);
+
+        % Reverse the normalization process
+        predictions = mapminmax('reverse', predictions,probs_out);
+        
+        % note here the usage of t the un normalized output in the 
+        % training dataset
         mse = perform(trainedNet, t, predictions);
 
         % Check if the current MSE is the best MSE so far and update the global variable if necessary.
